4303045d97e8c313eb3c902508c717b3c1a20bf7
natyosu3 <nachi33482@gmail.com>
2025-04-07 02:07:59 +0900

    アカウント作成画面でESCキーによるウェルカム画面への遷移を追加

--- screens/createAccount.go
+++ screens/createAccount.go
@@ -127,8 +127,13 @@ func (m CreateAccountModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "esc":
+		case "ctrl+c":
 			return m, tea.Quit
+
+		case "esc":
+			return m, func() tea.Msg {
+				return ScreenChangeMsg{Screen: "welcome"}
+			}
 
 		// Change cursor mode
 		case "ctrl+r":
